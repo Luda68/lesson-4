bc365e08eec92fa9778cee80b63ba82be02e894c
Luda68 <pril3pskay@yandex.ru>
2024-05-12 11:25:10 +0200

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,9 @@
+# my_string=(input("Всем привет! "))
+# name=len("Всем привет!")
+# print(name)
+
+print("my_string".upper())
+print("my_string".lower())
+print("my_string".replace("_",""))
+print("my_string".replace("my_string","m"))
+print("my_string".replace("my_string","g"))
